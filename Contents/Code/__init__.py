fc3b08f326403c1223d1fe0db646e42f8aecfc8c
sander1 <sanderspies+plexapp@gmail.com>
2010-12-29 22:57:59 +0100

    Update/rewrite by Raindancing

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,35 +1,37 @@
-#Rotten Tomatoes
-RT_SEARCH_URL = 'http://www.rottentomatoes.com/search/full_search.php?search=%s'
-RT_BASE_URL   = 'http://www.rottentomatoes.com'
+RT_IMDB_SEARCH_URL = 'http://www.rottentomatoes.com/alias?type=imdbid&s=%s'
 
 def Start():
-  HTTP.CacheTime = CACHE_1DAY
-  
+	HTTP.CacheTime = CACHE_1DAY
+
 class RottenTomatoesAgent(Agent.Movies):
-  name = 'Rotten Tomatoes'
-  languages = [Locale.Language.English]
-  primary_provider = False
-  contributes_to = ['com.plexapp.agents.imdb']
-  
-  def search(self, results, media, lang):
-    rtSearchResult = HTML.ElementFromURL(RT_SEARCH_URL % str(media.primary_metadata.id).replace('tt','')).xpath('//td[contains(@class,"firstCol") and @width="85%"]')
-    if len(rtSearchResult) > 0:
-      rtMovieUrl = rtSearchResult[0].xpath('p/a')[0].get('href')
-      #Log("**" + rtMovieUrl)
-      results.Append(
-        MetadataSearchResult(
-          id    = rtMovieUrl,
-          score = 100
-        )    
-      )
-    
-  def update(self, metadata, media, lang):
-    #load the rt movie page and grab the ratings
-    ratings = {}
-    for rating in HTML.ElementFromURL(RT_BASE_URL + metadata.id).xpath('//div[@class="movie_info_area"]//li/a'):
-      ratingText = rating.get('title')
-      if ratingText != "N/A" and len(ratingText) > 0:
-        ratings[rating.xpath('span')[0].text] = float(ratingText.replace('%',''))/10
-    metadata.rating = ratings['T-Meter Critics']
-    
-    
+	name = 'Rotten Tomatoes'
+	languages = [Locale.Language.English]
+	primary_provider = False
+	contributes_to = ['com.plexapp.agents.imdb']
+
+	def search(self, results, media, lang):
+		imdb_id = media.primary_metadata.id.replace('tt', '')
+		results.Append(MetadataSearchResult(id=imdb_id, score=100))
+
+	def update(self, metadata, media, lang):
+		movie_page = HTML.ElementFromURL(RT_IMDB_SEARCH_URL % metadata.id)
+		# Rating
+		try:
+			avg_rating = movie_page.xpath('//div[@id="all-critics-numbers"]/div/p/span')[0].text.split('/')[0]
+			metadata.rating = float(avg_rating)
+		except:
+			pass
+
+		# Summary
+		try:
+			metadata.summary = movie_page.xpath('//span[@id="movie_synopsis_all"]')[0].text
+		except:
+			pass
+
+		# Genres
+		metadata.genres.clear()
+		try:
+			for genre_elem in movie_page.xpath('//span[@property="v:genre"]'):
+				metadata.genres.add(genre_elem.text)
+		except:
+			pass
