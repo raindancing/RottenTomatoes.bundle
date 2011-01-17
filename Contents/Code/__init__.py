4a340f60b4332958d86a7735753fd2bb595fd512
sander1 <sanderspies+plexapp@gmail.com>
2011-01-17 19:34:37 +0100

    Yes/No preferences changed to booleans, added option to skip rating, formatted all xpaths the same way

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,48 +1,45 @@
-RT_SEARCH_URL = 'http://www.rottentomatoes.com/search/full_search.php?search=%s'
-RT_BASE_URL	 = 'http://www.rottentomatoes.com'
 RT_IMDB_SEARCH_URL = 'http://www.rottentomatoes.com/alias?type=imdbid&s=%s'
 
 def Start():
 	HTTP.CacheTime = CACHE_1DAY
-	
+
 class RottenTomatoesAgent(Agent.Movies):
 	name = 'Rotten Tomatoes'
 	languages = [Locale.Language.English]
 	primary_provider = False
 	contributes_to = ['com.plexapp.agents.imdb']
-	
+
 	def search(self, results, media, lang):
 		imdb_id = media.primary_metadata.id.replace('tt', '')
 		results.Append(MetadataSearchResult(id=imdb_id, score=100))
-	
+
 	def update(self, metadata, media, lang):
 		movie_page = HTML.ElementFromURL(RT_IMDB_SEARCH_URL % metadata.id)
-		try:
-			avg_rating = float(movie_page.xpath("id('all-critics-numbers')/div/p/span")[0].text.split('/')[0])
-		except:
-			avg_rating = None
-		if Prefs["rating_type"] == "Tomatometer":
+
+		if Prefs["get_rating"]:
 			try:
-				tomatometer = movie_page.xpath('//span[@id="all-critics-meter"]')[0].text
-				metadata.rating = float(tomatometer)/10
+				avg_rating = float(movie_page.xpath('//div[@id="all-critics-numbers"]/div/p/span')[0].text.split('/')[0])
 			except:
+				avg_rating = None
+			if Prefs["rating_type"] == "Tomatometer":
+				try:
+					tomatometer = movie_page.xpath('//span[@id="all-critics-meter"]')[0].text
+					metadata.rating = float(tomatometer)/10
+				except:
+					if avg_rating != None:
+						metadata.rating = avg_rating
+			else:
 				if avg_rating != None:
 					metadata.rating = avg_rating
 		else:
-			if avg_rating != None:
-				metadata.rating = avg_rating
-		
+			metadata.rating = None
+
 		metadata.genres.clear()
-		if Prefs["get_genres"] == "Yes":
+		if Prefs["get_genres"]:
 			for genre_elem in movie_page.xpath('//span[@property="v:genre"]'):
 				metadata.genres.add(genre_elem.text)
-		
-		if Prefs["get_summary"] == "Yes":
-			metadata.summary = movie_page.xpath("id('movie_synopsis_all')")[0].text
+
+		if Prefs["get_summary"]:
+			metadata.summary = movie_page.xpath('//span[@id="movie_synopsis_all"]')[0].text
 		else:
 			metadata.summary = ''
-		
-		
-		
-		
-		
